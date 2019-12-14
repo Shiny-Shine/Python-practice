afff88ae52dc08adc1b558166834826eb443bbbe
SHINE <zero777960@gmail.com>
2019-12-14 23:19:05 +0900

    2019-12-14 Study

---
+++ for loop.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+'''
+    2019-12-14 Python Study
+
+    for loop
+'''
+
+for b in range(5):
+	print(b)
+	print(b ** 2)   #bÀÇ 2Á¦°ö
+
+print()
+sum = 0
+for x in range(1, 11):
+    sum += x
+    print(x)
+print(sum)
