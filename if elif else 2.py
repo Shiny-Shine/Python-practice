8d142dc24382e6ae99065aefa7e0399ccf24e516
SHINE <zero777960@gmail.com>
2019-12-16 18:24:09 +0900

    2019-12-16 Study

--- if elif else 2.py
+++ if elif else 2.py
@@ -1,10 +1,10 @@
 #-*- coding: utf-8 -*-
 '''
-    2019-12-15 Python Study
+    2019-12-15 ~ 16 Python Study
 
     if elif else 2
 
-    연산자 따라 논리 연산을 수행한다.
+    세가지의 논리 연산자이다, 아래 규칙에 따라 논리적인 연산을 수행한다.
 
     논리 연산자 and             논리 연산자 or            논리 연산자 not
     True and True : 참          True or True : 참         not True : 거짓 
@@ -13,3 +13,18 @@
     False and False : 거짓      False or False : 거짓
 '''
 
+# and 연산
+if True and True : print("code 1\n")
+if True and False : print("code 2")
+if False and True : print("code 3")
+if False and False : print("code 4")
+
+# or 연산
+if True or True : print("code 1")
+if True or False : print("code 2")
+if False or True : print("code 3\n")
+if False or False : print("code 4")
+
+# not 연산
+if not True : print("code 1")
+if not False : print("code 2\n")
